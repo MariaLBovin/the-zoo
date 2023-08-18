84946b525c200e04687812dbcc6fb9728aa5c19e
MariaLBovin <maria.bovin@gmail.com>
2023-08-18 10:04:49 +0200

    fetch single animal added

---
+++ src/components/Animal.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import { IAnimal } from '../models/IAnimal'
+import { fetchAnimal } from '../services/animalService'
+
+export const Animal = () => {
+  const [animal, setAnimal] = useState<IAnimal | undefined>()
+
+    const {id} = useParams<{id:string}>();
+
+    useEffect(() => {
+    const fetchSingleAnimal = async () => {
+      try {
+        // Hämta djurdata från localStorage om det finns
+        const storedData = localStorage.getItem("animalsData");
+        if (storedData) {
+          const animals: IAnimal[] = JSON.parse(storedData);
+          const singleAnimal = animals.find((animal) => animal.id.toString() === id);
+          if (singleAnimal) {
+            setAnimal(singleAnimal);
+            console.log('Animal from localStorage');
+            
+          } else {
+            console.log("No single animal found in LS");
+          }
+        } else {
+          if(id){
+            const apiResponse = await fetchAnimal(id); 
+            setAnimal(apiResponse);
+            console.log("Animal from API");
+          }
+          else {
+            console.log('no id found');
+            
+          }
+        }
+      } catch (error) {
+        console.log("Error fetching single animal");
+      }
+    };
+    fetchSingleAnimal();
+
+  }, [id]);
+
+  return (
+    <>
+    <h3>{animal?.name}</h3>
+    <img src={animal?.imageUrl} alt={animal?.name} style={{ width: '200px', height: 'auto' }}/>
+    <div>{animal?.shortDescription}</div>
+    <button>Mata</button>
+    </>
+  )
+}
+
+export default Animal
