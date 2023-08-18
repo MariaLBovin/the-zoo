3c096ead19cd5f326565cdbd62ee06daa32e2eff
MariaLBovin <maria.bovin@gmail.com>
2023-08-18 10:04:36 +0200

    fetch all animals added

---
+++ src/components/Animals.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react"
+import { IAnimal } from "../models/IAnimal"
+import { Link } from "react-router-dom"
+import { fetchAnimals } from "../services/animalService"
+
+
+export const Animals = () => {
+    const [animals, setAnimals] =useState<IAnimal[]>([])
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          // Försök hämta datan från localStorage
+          const storedData = localStorage.getItem('animalData');
+  
+          if (storedData) {
+            // Om datan finns i localStorage, använd den
+            setAnimals(JSON.parse(storedData));
+            console.log('data från localStorage');
+            
+          } else {
+            // Om datan inte finns i localStorage, hämta från API
+            const response = await fetchAnimals();
+            setAnimals(response);
+  
+            // Spara datan i localStorage
+            localStorage.setItem('animalData', JSON.stringify(response));
+            console.log('data från API');
+            
+          }
+        } catch (error) {
+          console.log('Error fetching and saving data:', error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+  return (
+    <>
+    {animals.map((animal) => (
+        <div key={animal.id}>
+            <h3>{animal.name}</h3>
+            <img src={animal.imageUrl} alt={animal.name} style={{ width: '200px', height: 'auto' }} />
+            <Link to={'/animal/' + animal.id}>
+              <button>Läs mer</button>
+            </Link>
+        </div>
+    ))}
+    </>
+  )
+}
+
+export default Animals
